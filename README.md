7f628e1fa3fd7c6bc5cc8eabdc3a332f3c1daae5
harikun <cipto.suheri@gmail.com>
2022-10-24 23:10:53 +0700

    commit pertama

---
+++ README.md
@@ -0,0 +1,5 @@
+to build css:
+npx tailwindcss -i ./src/css/input.css -o ./public/css/style.css --watch
+
+open live server VC code extension to preview
+http://127.0.0.1:5500/public/index.html
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#B6639B",
+
+          secondary: "#483D7F",
+
+          white: "#FFFFFF",
+
+          accent: "#1FB2A6",
+
+          neutral: "#191D24",
+
+          "base-100": "#2A303C",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+
+          purple: "#474444",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
+};
